d587437348a85c898e357c6e28d88fad8ea36620
pozterz <pozterz2@gmail.com>
2017-10-07 22:31:26 +0700

    remove bar graph

--- src/template/src/components/barGraph.js
+++ src/template/src/components/barGraph.js
@@ -1,31 +1,6 @@
 import React from 'react'
 import { Bar } from 'react-chartjs-2'
 
-let graph = {
-    labels: [],
-    datasets: [{
-        label: 'Hours',
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-        data: []
-    }]
-}
-
 export default class BarGraph extends React.Component {
 
     constructor(props) {
