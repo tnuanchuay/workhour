826acecccff283c2cc154b20098d51e5a0fa921c
pozterz <pozterz2@gmail.com>
2017-10-07 18:14:40 +0700

    apply with real data

--- src/template/src/components/statistics.js
+++ src/template/src/components/statistics.js
@@ -5,7 +5,20 @@ import Graph from './graph'
 
 // sample data
 let jsonData = {
-  data: [3,2,3,4,5,6,7,8,9]
+  data: [
+    {
+      "_id" : ObjectId("59d08f4131ad137215815fd4"),
+      "endTime" : NumberLong(1506840385898),
+      "session" : "fe878058a3816944cfe0e5677eb7f7d9334dd78fa17c9aeb146555d4cd74b040dbdebb0df30949c000160ce92ce477132035aa2fb18ced97bfe5e462d5ff2816",
+      "startTime" : NumberLong(1506782500413)
+    },
+    {
+        "_id" : ObjectId("59d22b8e31ad13721581bab0"),
+        "session" : "fe878058a3816944cfe0e5677eb7f7d9334dd78fa17c9aeb146555d4cd74b040dbdebb0df30949c000160ce92ce477132035aa2fb18ced97bfe5e462d5ff2816",
+        "startTime" : NumberLong(1506913457109),
+        "endTime" : NumberLong(1506945934765)
+    }
+  ]
 }
 
 export default class Statistic extends React.Component {
@@ -17,6 +30,12 @@ export default class Statistic extends React.Component {
       data: [],
       session: session
     }
+  }
+
+  processData(data) {
+    const processedData = data.map( rec => {
+      
+    })
   }
 
   componentDidMount() {
